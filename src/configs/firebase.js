88c1d3a3bf23e02805b0257b4bb35598d055eeb4
Vinícius Pacheco Vieira <vinipachecov@gmail.com>
2018-06-10 21:15:33 -0300

    Delete firebase.js

--- src/configs/firebase.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  apiKey: "AIzaSyCDmJUtXMHid784hYOiDY7E22e1taoXguY",
-  authDomain: "church-library-bc4ae.firebaseapp.com",
-  databaseURL: "https://church-library-bc4ae.firebaseio.com",
-  projectId: "church-library-bc4ae",
-  storageBucket: "church-library-bc4ae.appspot.com",
-  messagingSenderId: "998981175828"
-};
